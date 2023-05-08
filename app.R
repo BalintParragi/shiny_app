22f31de46ca35d3578da57c954d1f02ebe5b199e
BalintParragi <parragi.balint@gmail.com>
2023-05-08 11:55:49 +0200

    Finish edits on the app

--- app.R
+++ app.R
@@ -9,194 +9,170 @@ library(plotly)
 library(shinythemes)
 library(shinydashboard)
 library(markdown)
-
+library(readxl)
+library(stringr)
+library(tidyverse)
+library(rvest)
 source('functions.R')
 
-#aims: dropdown menu, date picker, table, graph
-
-# ui <- fluidPage(
-#   uiOutput('my_ticker'),
-#   textOutput('ticker_id'),
-#   dateRangeInput("my_date",label = h3("Date range"),start = Sys.Date()-365,
-#                  end = Sys.Date()),
-#   #plot
-#   plotlyOutput('data_plot',width = '80%',height = '600px'),
-#   #df
-#   dataTableOutput('my_data')
-# )
-
-#ctrl shift c
-
-# ui <- fluidPage(
-#   sidebarLayout(
-#     sidebarPanel(
-#       uiOutput('my_ticker'),
-#         textOutput('ticker_id'),
-#         dateRangeInput("my_date",label = h3("Date range"),start = Sys.Date()-365,
-#                        end = Sys.Date()),
-#       tags$a(href="https://www.tradingview.com", "Data is available here.")
-#       
-#     ),
-#     mainPanel(
-#       tabsetPanel(
-#       tabPanel("Plot",plotlyOutput('data_plot',width = '80%',height = '600px')),
-#       tabPanel("Table",dataTableOutput('my_data'))
-#       )
-#     )
-#   )
-#   
-# )
-
-
-# ui <- fluidPage(
-#     mainPanel(
-#       tabsetPanel(
-#         tabPanel("Control",
-#                  wellPanel(uiOutput('my_ticker'),
-#                            textOutput('ticker_id'),
-#                            dateRangeInput("my_date",label = h3("Date range"),start = Sys.Date()-365,
-#                          end = Sys.Date()),
-#                          tags$a(href="https://www.tradingview.com", "Data is available here."))),
-#         
-#         tabPanel("Plot",plotlyOutput('data_plot',width = '80%',height = '600px')),
-#         tabPanel("Table",dataTableOutput('my_data')),
-#         
-#       )
-#     )
-#   )
-
-# ui <- navbarPage(title = "Stock app", theme = shinytheme("united"),
-#                  tabPanel("Control",
-#                           wellPanel(uiOutput('my_ticker'),
-#                                     textOutput('ticker_id'),
-#                                     dateRangeInput("my_date",label = h3("Date range"),start = Sys.Date()-365,
-#                                                    end = Sys.Date()),
-#                                     tags$a(href="https://www.tradingview.com", "Data is available here."))),
-#                  tabPanel("Plot",plotlyOutput('data_plot',width = '80%',height = '600px')),
-#                  tabPanel("Table",dataTableOutput('my_data')))
-
-# ui <- navbarPage(title = "Stock app", theme = shinytheme("united"),
-#                  tabPanel("Control",
-#                           wellPanel(uiOutput('my_ticker'),
-#                                     textOutput('ticker_id'),
-#                                     dateRangeInput("my_date",label = h3("Date range"),start = Sys.Date()-365,
-#                                                    end = Sys.Date()),
-#                                     tags$a(href="https://www.tradingview.com", "Data is available here."))),
-#                  tabPanel("Plot and data",
-#                           fluidRow(
-#                             column(width = 9,plotlyOutput('data_plot',width = '75%',height = '600px')),
-#                             column(width = 3,dataTableOutput('my_data',width = '25%')))))
-
-# ui <- dashboardPage(
-#   dashboardHeader(title = "Stock app"),
-#   dashboardSidebar(uiOutput('my_ticker'),
-#                    textOutput('ticker_id'),
-#                    dateRangeInput("my_date",label = h3("Date range"),start = Sys.Date()-365,
-#                                                      end = Sys.Date()),
-#                    tags$a(href="https://www.tradingview.com", "Data is available here.")),
-#   dashboardBody(tabPanel(
-#              fluidRow(
-#                column(width = 12,textOutput('my_ticker'))),
-#              fluidRow(
-#                column(width = 12,plotlyOutput('data_plot',width = '100%',height = '600px'))),
-#              fluidRow(
-#                column(width = 12,"Raw data\n")),
-#              fluidRow(
-#                column(width = 12,dataTableOutput('my_data',width = '100%'))))
-#     
-#   )
-# )
+Sys.setlocale(locale= "English")
 
 ui <- dashboardPage(
-  dashboardHeader(title = "Stock app"),
+  skin = "red",
+  dashboardHeader(title = "NBA Standings"),
   dashboardSidebar(sidebarMenu(
     menuItem("Dashboard", tabName = "dashboard", icon = icon("home", lib = "glyphicon")),
-    menuItem("Plot", tabName = "plot", icon = icon("signal", lib = "glyphicon")),
-    menuItem("Data",tabName = "data",icon = icon("list-alt", lib = "glyphicon")),
-    menuItem("Info box",tabName = "infobox",icon = icon("fire", lib = "glyphicon")),
-    menuItem("Value box",tabName = "valuebox",icon = icon("usd", lib = "glyphicon"))
+    menuItem("Standing trajectories", tabName = "plot", icon = icon("signal", lib = "glyphicon")),
+    menuItem("Standings table",tabName = "data",icon = icon("list-alt", lib = "glyphicon")),
+    menuItem("Best record",tabName = "infobox",icon = icon("fire", lib = "glyphicon"))
   )),
   dashboardBody(
     tabItems(
       tabItem(tabName = "dashboard",
               fluidRow(
                 box(title = "Controls",
-                    uiOutput('my_ticker'),
-                    textOutput('ticker_id'),
-                    dateRangeInput("my_date",label = h3("Date range"),start = Sys.Date()-365,
-                                   end = Sys.Date()),
-                    tags$a(href="https://www.tradingview.com", "Data is available here.")))
+                    uiOutput('my_ticker_e'),
+                    uiOutput('my_ticker_w'),
+                    uiOutput('my_slider'),
+                    uiOutput('my_date'),
+                    p("Description: regular season NBA data from 2005 until 2019 covering team standings by day."),
+                    p("Note: a regular season usually runs from November until mid-April."),
+                    tags$a(href="https://www.basketball-reference.com", "Data is obtained via scraping from here.")))
       ),
       tabItem(tabName = "plot",
-              h2("Stock trajectory"),
-              fluidRow(plotlyOutput('data_plot',width = '100%',height = '600px'))
+              h2("Season standings after 1st of January\n"),
+              h3("Eastern conference\n"),
+              fluidRow(plotlyOutput('data_plot_east',width = '100%',height = '600px')),
+              h3("Western conference\n"),
+              fluidRow(plotlyOutput('data_plot_west',width = '100%',height = '600px'))
       ),
       tabItem(tabName = "data",
-              h2("Raw data"),
-              fluidRow(dataTableOutput('my_data',width = '100%'))
+              h2("Daily standings"),
+              fluidRow(column(width = 6,dataTableOutput('my_data_east',width = '100%')),
+                       column(width = 6,dataTableOutput('my_data_west',width = '100%')))
       ),
       tabItem(tabName = "infobox",
-              h2("On fire"),
+              h2("Teams with best record"),
               fluidRow(
-                infoBoxOutput("pos_closing_share")
-              ),
-      ),
-      tabItem(tabName = "valuebox",
-              fluidRow(
-                valueBoxOutput("market_cap")
+                infoBoxOutput("pos_record_east"),
+                infoBoxOutput("pos_record_west")
               ),
       )
-      
     )
   )
 )
 
 server <- function(input, output, session) {
-  sp500 <-data.table(get_sp500())
+  standings_west <-data.table(get_standings_colors("west"))
+  standings_east <-data.table(get_standings_colors("east"))
   
-  setorder(sp500, description)#-market_cap_basic
   
-  output$pos_closing_share <- renderInfoBox({
-    infoBox(
-      "Closing\npositive", paste0(round(nrow(sp500[change>0,])/nrow(sp500),
-                                        digits = 2)*100,"%"),
-      color = ifelse(nrow(sp500[change>0,])/nrow(sp500)>0.5,"green","red")
-    )
+  output$my_ticker_e <- renderUI({
+    selectInput('my_ticker_e', label = 'Select an eastern team', choices = setNames(sort(unique(standings_east$team)),
+                                                                        sort(unique(standings_east$team))), multiple = FALSE)
   })
   
-  output$market_cap <- renderValueBox({
-    valueBox(
-      paste0(round(sum(sp500$market_cap_basic)/1e9,digits = 0)," Billion USD"), 
-      "Market value",
-      color = "orange"
-    )
+  output$my_ticker_w <- renderUI({
+    selectInput('my_ticker_w', label = 'Select a western team', choices = setNames(sort(unique(standings_west$team)),
+                                                                           sort(unique(standings_west$team))), multiple = FALSE)
   })
   
-  output$my_ticker <- renderUI({
-    selectInput('ticker', label = 'Select a ticker', choices = setNames(sp500$name, sp500$description), multiple = FALSE)
+  output$my_slider <- renderUI({
+    sliderInput('my_slider', label = 'Select a season',
+                min = min(standings_west$season),
+                max = max(standings_west$season),
+                value = min(standings_west$season),
+                sep = "")
   })
-  output$ticker_id <- renderText(paste0("Company ID: ",input$ticker))
   
+  output$my_date <- renderUI({
+    dateInput('my_date',label = "Select a day within that season", 
+                       value = min(standings_west$date_new))
+    
+  })
   
-  my_reactive_df <- reactive({
-    df<- get_data_by_ticker_and_date(input$ticker, input$my_date[1], input$my_date[2])
-    df <- data.frame(lapply(df, function(y) if(is.numeric(y)) round(y, 2) else y)) 
+  # observeEvent(output$my_slider, {
+  #   year <- output$my_slider
+  #   start_date <- as.Date(paste0(year-1, "-10-01"))
+  #   end_date <- as.Date(paste0(year, "-05-01"))
+  #   updateDateInput(session, "my_date", minDate = start_date, maxDate = end_date, value = as.Date(start_date+30))
+  # })
+  
+  my_reactive_df_east <- reactive({
+    df<- get_data_by_slider_and_date(input$my_date, input$my_slider,"east")
+    return(df)
+  })
+  
+  my_reactive_df_west <- reactive({
+    df<- get_data_by_slider_and_date(input$my_date, input$my_slider,"west")
+    return(df)
+  })
+  
+  output$my_data_east <- DT::renderDataTable(server = FALSE, {
+    render_df_with_all_download_buttons(my_reactive_df_east())
+    
+  })
+  
+  output$my_data_west <- DT::renderDataTable(server = FALSE, {
+    render_df_with_all_download_buttons(my_reactive_df_west())
+    
+  })
+  
+  #Plot - slightly different data is necessary
+  
+  my_reactive_dfplot_east <- reactive({
+    df<- get_data_by_slider_and_ticker(input$my_ticker_e, input$my_slider,"east")
+    return(df)
+  })
+  
+  my_reactive_dfplot_west <- reactive({
+    df<- get_data_by_slider_and_ticker(input$my_ticker_w, input$my_slider,"west")
     return(df)
   })
   
   
-  # go to https://rstudio.github.io/DT/shiny.html
-  # output$my_data <- DT::renderDataTable({
-  #   render_df_with_buttons(my_reactive_df())
-  # })
   
-  output$my_data <- DT::renderDataTable(server = FALSE, {
-    render_df_with_all_download_buttons(my_reactive_df())
-    
+  output$data_plot_west <- renderPlotly({
+    get_plot_of_data(my_reactive_dfplot_west())
   })
   
-  output$data_plot <- renderPlotly({
-    get_plot_of_data(my_reactive_df())
+  output$data_plot_east <- renderPlotly({
+    get_plot_of_data(my_reactive_dfplot_east())
+  })
+  
+  # team_max_win <- reactive({
+  #   team_max_win_val = ifelse(as.numeric(my_reactive_df_east()$`Win PCT`)>
+  #                            as.numeric(my_reactive_df_west()$`Win PCT`),
+  #                          my_reactive_df_east()$Team[1],
+  #                          my_reactive_df_west()$Team[1])
+  #   
+  #   return(team_max_win_val)
+  #   
+  # })
+  
+  
+  output$pos_record_east <- renderInfoBox({
+    infoBox(
+      "Best eastern record",
+      ifelse(nrow(my_reactive_df_east())>0,paste0(my_reactive_df_east()$Team[1],":\n",
+             round(max(as.numeric(my_reactive_df_east()$`Win PCT`)),
+                                             digits = 2)*100,"%"),
+             "No data"),
+      color = "green",
+      icon = icon("chevron-right", lib = "glyphicon")
+    )
+  })
+  
+  output$pos_record_west <- renderInfoBox({
+    infoBox(
+      "Best western record",
+      ifelse(nrow(my_reactive_df_west())>0,paste0(my_reactive_df_west()$Team[1],":\n",
+             round(max(as.numeric(my_reactive_df_west()$`Win PCT`)),
+                   digits = 2)*100,"%"),
+             "No data"),
+      color = "green",
+      icon = icon("chevron-left", lib = "glyphicon")
+    )
   })
   
 }
--- functions.R
+++ functions.R
@@ -1,58 +1,129 @@
-get_tradingview_data <- function(json_string) {
-  headers = c(
-    `authority` = 'scanner.tradingview.com',
-    `accept` = 'text/plain, */*; q=0.01',
-    `origin` = 'https://www.tradingview.com',
-    `user-agent` = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
-    `content-type` = 'application/x-www-form-urlencoded; charset=UTF-8',
-    `sec-fetch-site` = 'same-site',
-    `sec-fetch-mode` = 'cors',
-    `referer` = 'https://www.tradingview.com/',
-    `accept-encoding` = 'gzip, deflate, br',
-    `accept-language` = 'hu-HU,hu;q=0.9,en-US;q=0.8,en;q=0.7'
-  )
+
+get_standings_colors <- function(conf) {
+  df <- read_excel(paste0("data/standing_",conf,"_byday.xlsx")) %>% 
+    mutate(conference = conf) %>% 
+    filter(season<=2019)
+  df <- df %>% 
+    mutate(date_new = as.Date(paste(str_sub(date,-4),
+                                  match(str_sub(date,1,3),month.abb),
+                                  str_sub(date,5,-7),sep="-"))) 
   
-  res <- httr::POST(url = 'https://scanner.tradingview.com/america/scan', httr::add_headers(.headers=headers), body = json_string)
+  trim_col <- names(df)[grepl("\\d", names(df))]
+  df <- df %>% 
+    mutate_at(trim_col, funs(gsub("\\).*", ")", .))) %>% 
+    select(-date) %>% 
+    data.table()
   
-  t <- fromJSON(content(res, 'text'))
-  df_data <-
-    rbindlist(lapply(t$data$d, function(x){
-      data.frame(t(data.frame(x)), stringsAsFactors = F)
-    }))
+  df_melt <- melt(df,id.vars=c(16:18)) %>% 
+    arrange(season,conference,date_new) %>% 
+    mutate(seed = as.numeric(gsub("[a-z]","",variable)),
+           team = str_sub(value,1,3),
+           standing = gsub("\\(([^)]+)\\)", "\\1",str_sub(value,5))) %>% 
+    tidyr::separate(standing,into = c("wins","losses"),sep = "-") %>% 
+    select(-value,variable) %>% 
+    mutate(wins = as.numeric(wins),
+           losses = as.numeric(losses)) %>% 
+    group_by(season) %>% 
+    mutate(reg_season = last(wins)+last(losses)) %>% 
+    ungroup() %>% 
+    rowwise() %>% 
+    mutate(matches_left = reg_season-wins-losses) %>%
+    data.table()
   
-  names(df_data) <-  fromJSON(json_string)$columns
-  final_data <- cbind( data.table('exchange' = sapply(strsplit(t$data$s, ':'), '[[', 1)),  df_data)
-  return(final_data)
+  colors <- read_html("https://teamcolorcodes.com/nba-team-color-codes/") %>% 
+    html_nodes("table") %>% html_table()
+  colors <- colors[[4]] %>% 
+    rename(team = `NBA Team Name`,
+           color1 = `Color 1`,
+           color2 = `Color 2`) %>% 
+    dplyr::select(c(team,color1,color2))
+  edit_col <- names(colors)[grepl("\\d", names(colors))]
+  colors_edit <- colors %>% 
+    mutate_at(edit_col, funs(gsub(".*\\#", "#", .))) %>% 
+    mutate(code= toupper(str_sub(team,1,3)),
+           code = ifelse(team=="Los Angeles Lakers","LAL",
+                         ifelse(team=="Los Angeles Clippers","LAC",
+                                ifelse(team=="San Antonio Spurs","SAS",
+                                       ifelse(team=="Golden State Warriors","GSW",
+                                              ifelse(team=="New Jersey Nets","NJN",
+                                                     ifelse(team=="New York Knicks","NYK",
+                                                            ifelse(team=="Oklahoma City Thunder","OKC",
+                                                                   ifelse(team=="New Orleans Pelicans","NOP",
+                                                                          ifelse(team=="Brooklyn Nets","BRK",
+                                                                                 ifelse(team=="New Orleans Hornets","NOH",
+                                                                                        ifelse(team=="New Orleans/Oklahoma City Hornets", "NOK",
+                                                                                               ifelse(team =="Charlotte Hornets","CHO",code)))))))))))))
   
+  colors_edit <- colors_edit %>% 
+    #add teams that have changed name/location and not present anymore
+    add_row(team = "New Orleans Hornets",color1 = NA,
+            color2 = NA,code = "NOH") %>% 
+    add_row(team = "New Orleans/Oklahoma City Hornets",color1 = NA,
+            color2 = NA,code = "NOK") %>% 
+    mutate(color1 = ifelse(is.na(color1),color1[code == "NOP"],color1),
+           color2 = ifelse(is.na(color2),color2[code == "NOP"],color2)) %>% 
+    add_row(team = "Charlotte Hornets",color1 = NA,
+            color2 = NA,code = "CHA") %>% 
+    mutate(color1 = ifelse(is.na(color1),color1[code == "CHO"],color1),
+           color2 = ifelse(is.na(color2),color2[code == "CHO"],color2)) %>% 
+    add_row(team = "Seattle Supersonics",color1 ="#00653A",color2="#FFC200",code = "SEA") %>% 
+    add_row(team = "New Jersey Nets",color1 = NA,
+            color2 = NA,code = "NJN") %>% 
+    mutate(color1 = ifelse(is.na(color1),color1[code == "BRK"],color1),
+           color2 = ifelse(is.na(color2),color2[code == "BRK"],color2))
+  
+  df_melt_colors <- df_melt %>% 
+    rename(code = team) %>% 
+    left_join(colors_edit,by="code") %>% data.table()
+
+  return(df_melt_colors)
 }
 
-
-get_sp500 <- function() {
-  adat <- get_tradingview_data('{"filter":[{"left":"name","operation":"nempty"}],"options":{"lang":"en"},"symbols":{"query":{"types":[]},"tickers":[],"groups":[{"type":"index","values":["SP:SPX"]}]},"columns":["name","close","change","change_abs","Recommend.All","volume","market_cap_basic","price_earnings_ttm","earnings_per_share_basic_ttm","number_of_employees","sector","description","name","type","subtype","update_mode","pricescale","minmov","fractional","minmove2"],"sort":{"sortBy":"name","sortOrder":"asc"},"range":[0,700]}')
-  adat$market_cap_basic <- as.numeric(adat$market_cap_basic)
-  return(adat)
-}
-
-
-get_data_by_ticker_and_date  <- function(ticker, start_date, end_date) {
+get_data_by_slider_and_date  <- function(day, slider,conf) {
   
   tryCatch({
-    my_data <- data.frame(ds.getSymbol.yahoo(ticker, from = (as.Date(start_date) -365), to =end_date ))
-    names(my_data) <- tolower(sapply(strsplit(names(my_data), '.', fixed = T), '[[', 2))
-    my_data$date <- as.Date(row.names(my_data)) 
-    row.names(my_data) <- 1:nrow(my_data)
-    my_data <- data.table(my_data)
-    
-    if( !identical(names(my_data) , c("open","high","low", "close","volume",  "adjusted","date"))) {
-      text<- paste0('Error: ', my_ticker, ' # problem: names of dataframe is bad ', ' time: ', Sys.time())
-      stop(text)
-    }
-    my_data$rsi <- round(RSI(my_data[['close']]),2)
-    my_data[['sma_200_value']] <- SMA(my_data[['close']],200)
-    my_data[['sma_50_value']] <- SMA(my_data[['close']], 50)
-    my_data <- my_data[date>=start_date,]
+    my_data <- get_standings_colors(conf)
+    my_data <- my_data[date_new==as.Date(day)&season==slider,] %>% 
+      mutate(wins_share = wins/(wins+losses),
+             wins_share_char = ifelse(str_sub(round(wins_share,digits = 3),1,1)!="1",
+                                      str_sub(round(wins_share,digits = 3),2),
+                                      "1.000")) %>% 
+      rowwise() %>% 
+      mutate(wins_share_char = ifelse(nchar(wins_share_char)<4,
+                                 paste0(wins_share_char,paste0(rep("0",4-nchar(wins_share_char)),collapse="")),
+                                 wins_share_char),
+             wins_share_char = ifelse(wins_share_char=="0000","0.000",wins_share_char)) %>% 
+      dplyr::select(c(variable,team,wins,losses,wins_share_char,matches_left)) %>% 
+      rename(Wins = wins,
+             Losses = losses,
+             `Win PCT` = wins_share_char,
+             Team = team,
+             Seed = variable,
+             `Matches left` = matches_left)
     
     if ( nrow(my_data[complete.cases(my_data)==F,])> 0)  {
+      my_data <- my_data[complete.cases(my_data),]
+      if(nrow(my_data)==0){
+        text<- paste0('Error: ', my_slider, ' # problem: empty dataframe ', ' time: ', Sys.time())
+        stop(text)
+      }
+    }
+    return(my_data)
+  }, error=function(x) {
+    print(x)
+    return(data.table())
+  })
+  
+}
+
+get_data_by_slider_and_ticker  <- function(ticker, slider,conf) {
+  
+  tryCatch({
+    my_data <- get_standings_colors(conf)
+    my_data <- my_data[season==slider&date_new>=as.Date(paste0(slider,"-01-01")),] %>% 
+      mutate(thick = ifelse(team == ticker,1.5,0.6))
+    
+    if(nrow(my_data[complete.cases(my_data)==F,])> 0)  {
       my_data <- my_data[complete.cases(my_data),]
       if(nrow(my_data)==0){
         text<- paste0('Error: ', my_ticker, ' # problem: empty dataframe ', ' time: ', Sys.time())
@@ -69,65 +140,59 @@ get_data_by_ticker_and_date  <- function(ticker, start_date, end_date) {
 
 
 render_df_with_buttons <- function(my_data) {
-  # https://rstudio.github.io/DT/extensions.html
   return(DT::datatable(my_data,extensions = c('Buttons','FixedHeader'),filter = 'top', class = 'cell-border stripe',
+                       rownames = FALSE,
                        options = list(dom = 'Blfrtip',scrollX = TRUE, fixedHeader = TRUE,
-                                      pageLength = 10,lengthMenu = c(10,50, 100, 500, 50000 ),
+                                      pageLength = 15,lengthMenu = c(5, 10, 15),
                                       buttons = c('copy', 'csv', 'excel', 'pdf', 'print'))))
   
 }
 
-
-
-
 render_df_with_all_download_buttons <- function(my_data) {
-  # https://rstudio.github.io/DT/extensions.html
   return(DT::datatable(my_data,extensions = c('Buttons','FixedHeader'),filter = 'top', class = 'cell-border stripe',
+                       rownames= FALSE,
                        options = list(dom = 'Blfrtip',scrollX = TRUE, fixedHeader = TRUE,
-                                      pageLength = 10,lengthMenu = c(10,50, 100, 500, 50000 ),
+                                      pageLength = 15,lengthMenu = c(5,10,15),
                                       buttons = list(list(extend = "excel", text = "Download", 
-                                                          filename =  paste0('stock-data-', Sys.Date()),  exportOptions = list(modifier = list(page = "all"))))
+                                                          filename =  paste0('standings-data-', Sys.Date()),  exportOptions = list(modifier = list(page = "all"))))
                        )))
   
 }
 
 
-
-
-
-get_plot_of_data  <- function(data) {
+get_plot_of_data <- function(data){
   tryCatch({
-    data$fall_from_max <- ((data$low / max(data$high)) -1 )*100
-    data$fall_from_close <- ((data$close / max(data$high)) -1 )*100
-    data$fall_from_high <- ((data$high / max(data$high)) -1 )*100
-    
-    
-    
-    fig <-plot_ly(data, x=~date, type="candlestick",
-                  open = ~open, close = ~close,
-                  high = ~high, low = ~low) %>% 
-      layout(title = "Basic Candlestick Chart", showlegend = FALSE,  xaxis = list(rangeslider = list(visible = F))) %>% 
-      add_lines(x=~date,y=~sma_200_value, name='200 daily MA') %>% 
-      add_lines(x=~date,y=~sma_50_value, name='50 daily MA') 
-    
-    
-    rsi_plot <- 
-      plot_ly(data, x=~date,y=~rsi,  type = 'scatter', mode = 'lines', name = 'min') %>% 
-      add_trace(x=~date,y=30, line = list(color = 'red', width = 4, dash = 'dash') ) %>% 
-      add_trace(x=~date,y=70, line = list(color = 'red', width = 4, dash = 'dash')) %>% 
-      layout(title = "", showlegend = F,  xaxis =list(title=""), yaxis =list(title="RSI") )
-    
-    all_p<- subplot(list(fig, rsi_plot ), nrows = 2, shareX = T, shareY = F, margin = 0.01, heights = c(0.75, 0.25))
-    return(all_p)
-    
+    p <- ggplot(mapping = aes(text = paste("Date: ", date_new,
+                                 "<br>Team: ", code,
+                                 "<br>Seed: ", seed))) +
+      geom_line(data = data,
+                aes(x = date_new, y = seed, group = code,
+                    color = color1, size = thick)) +
+      geom_line(data = data,
+                aes(x = date_new, y = seed-0.1, group = code,
+                    color = color2, size = thick/2)) +
+      geom_text(data = data[date_new==max(date_new),],
+                mapping=aes(x=date_new+2, y=seed,
+                            label=code,color = color1), hjust = 0, size = 3.25)+
+      theme_minimal()+
+      labs(title = "Selected team is highlighted",x = "", y = "Seed") +
+      theme(axis.text = element_text(size = 13, colour = "black"),
+            axis.title = element_text(size = 13, colour = "black"),
+            plot.title = element_text(hjust = 0.5, size = 14),
+            #panel.spacing = unit(0.8, "lines"),
+            panel.grid.major.y = element_blank(),
+            plot.margin = margin(0.25,0.25,0.25,0.25,unit = "cm"),
+            legend.position = "none")+
+      scale_color_identity()+
+      scale_size_identity()+
+      scale_y_reverse(breaks = 1:15)+
+      scale_x_date(date_labels = "%b",expand = c(0.025,0.025))
+    pl <- ggplotly(p,tooltip = 'text')
+    return(pl)
     
   },error=function(x){
     return(plotly_empty())
-  })
+    })
   
-}
-
-
-get_ggplot_plot <- function(df) {
-  ggplot(df, aes(date, close))+geom_line()+theme_bw()+labs(x='Date', y='Close price')
+  
 }
